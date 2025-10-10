259ea4f315638fdefd2b28a1d92ad3c7a4839a29
Hugo Viana <hugo@viana.fr>
2025-10-10 09:54:39 +0200

    Implement neural network model with training, validation, and test steps; add Python environment configuration

---
+++ model.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import lightning as L
+
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+class NN(L.LightningModule):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, 64),
+            F.ReLU(),
+            nn.Linear(64, 64),
+            F.ReLU()
+        )
+        self.out = nn.Linear(64, output_dim)
+
+        self.criterion = nn.MSELoss()
+        
+    
+    def forward(self, x):
+        x = self.layers(x)
+        x = self.out(x)
+        return x
+    
+    def training_step(self, batch, batch_idx):
+        loss, y, y_hat = self._common_step(batch, batch_idx)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        loss, y, y_hat = self._common_step(batch, batch_idx)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        loss, y, y_hat = self._common_step(batch, batch_idx)
+        return loss
+    
+    def _common_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.forward(x)
+        loss = self.criterion(y_hat, y)
+        self.log('loss', loss)
+        return loss, y, y_hat
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
