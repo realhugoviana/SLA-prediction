53ff3035d6160a1fd8bd94c0364468cc7ce8c373
Hugo Viana <hugo@viana.fr>
2025-11-20 10:44:03 +0100

    stats

--- stats_entrainement/stats.py
+++ stats_entrainement/stats.py
@@ -7,11 +7,51 @@ df_r2 = pd.read_csv("stats_entrainement/R2.csv", sep=';')
 df_rmse = pd.read_csv("stats_entrainement/RMSE.csv", sep=';')
 
 df_mae[['dataset', 'trial']] = df_mae['Run'].str.split('/', expand=True)
-df_mae.drop(columns=['Run'], inplace=True)
 df_r2[['dataset', 'trial']] = df_r2['Run'].str.split('/', expand=True)
-df_r2.drop(columns=['Run'], inplace=True)
 df_rmse[['dataset', 'trial']] = df_rmse['Run'].str.split('/', expand=True)
-df_rmse.drop(columns=['Run'], inplace=True)
 
+df_mae["Value"] = df_mae["Value"].str.replace(",", ".").astype(float)
+df_r2["Value"] = df_r2["Value"].str.replace(",", ".").astype(float)
+df_rmse["Value"] = df_rmse["Value"].str.replace(",", ".").astype(float)
 
-print(df_mae.head())
+df_mae.boxplot(column="Value", by="criterion", grid=False)
+
+df_mae.boxplot(column="Value", by="n_layer", grid=False)
+
+df_mae.boxplot(column="Value", by="n_units", grid=False)
+
+df_mae.boxplot(column="Value", by="optimizer", grid=False)
+
+df_mae.boxplot(column="Value", by="activation", grid=False)
+
+df_mae.boxplot(column="Value", by="decroissant", grid=False)
+
+df_mae.plot.scatter(x="learning_rate", y="Value")
+
+df_mae.boxplot(column="Value", by="learning_rate", grid=False)
+
+plt.show()
+
+def top5_by_dataset(df):
+    return (
+        df.sort_values(["dataset", "Value"])   
+          .groupby("dataset")
+          .head(5)                
+          .reset_index(drop=True)
+    )
+
+# Application aux trois métriques
+top5_mae = top5_by_dataset(df_mae)
+top5_r2 = top5_by_dataset(df_r2)
+top5_rmse = top5_by_dataset(df_rmse)
+
+# Affichage
+print("Top 5 MAE:")
+print(top5_mae)
+
+print("\nTop 5 R2:")
+print(top5_r2)
+
+print("\nTop 5 RMSE:")
+print(top5_rmse)
+
