659d1ad2cab0c717389cf51a22b2ffa81295792a
Hugo Viana <hugo@viana.fr>
2026-05-14 18:40:42 +0200

    Commentaires

--- MLP_regression/model.py
+++ MLP_regression/model.py
@@ -3,51 +3,59 @@ import torch.nn as nn
 import lightning as L
 import torchmetrics
 
+# Module lighting : pratique car pas besoin de coder à la main les boucles d'entrainement
 class MLP_regressor(L.LightningModule):
     def __init__(self, input_dim, output_dim, n_layer=2, n_units=16, learning_rate=1e-3, decroissant=False, activation='ReLU', optimizer='Adam', criterion='MSE'):
         super().__init__()
 
+        # Sauvegarde des paramètres
         self.save_hyperparameters()
 
-        self.lr = learning_rate
+        self.lr = learning_rate # Learning rate
 
+        # Sélection de la fonction d'activation en fonction de l'entrée
         self.activation_fn = {
             'ReLU': nn.ReLU(),
             'sigmoid': nn.Sigmoid(),
             'tanh': nn.Tanh()
         }[activation]
 
+        # Architecture
         self.layers = nn.Sequential()
-        self.layers.add_module('input_layer', nn.Linear(input_dim, n_units))
-        self.layers.add_module('input_activation', self.activation_fn)
+        self.layers.add_module('input_layer', nn.Linear(input_dim, n_units)) # Première couche 
+        self.layers.add_module('input_activation', self.activation_fn) # Couche d'activation
 
+        # Si décroissant, on divise par deux le nombre de neurone par couche à chaque couche
         if decroissant:
             in_units = n_units
             out_units = in_units // 2
-            for i in range(n_layer - 1):
+            for i in range(n_layer - 1): # On créer autant de couche que précisé en entrée
                 self.layers.add_module(f'hidden_layer_{i+1}', nn.Linear(in_units, out_units))
                 self.layers.add_module(f'hidden_activation_{i+1}', self.activation_fn)
                 in_units = out_units
                 out_units = in_units // 2
-            self.out = nn.Linear(out_units*2, output_dim)
+            self.out = nn.Linear(out_units*2, output_dim) # Dernière couche sans activation 
         else:
             for i in range(n_layer - 1):
                 self.layers.add_module(f'hidden_layer_{i+1}', nn.Linear(n_units, n_units))
                 self.layers.add_module(f'hidden_activation_{i+1}', self.activation_fn)
             self.out = nn.Linear(n_units, output_dim)
 
+        # Fonction de perte
         self.criterion = {
             'MSE': nn.MSELoss(),
             'MAE': nn.L1Loss(),
             'Huber': nn.SmoothL1Loss()
         }[criterion]
 
+        # Optimizer (déscente de gradient & retro-propagation)
         self.optimizer = {
             'Adam': torch.optim.Adam,
             'RMSprop': torch.optim.RMSprop,
             'Adagrad': torch.optim.Adagrad
         }[optimizer]
 
+        # Métriques : MAE, RMSE, R2
         self.train_mae = torchmetrics.MeanAbsoluteError()
         self.val_mae = torchmetrics.MeanAbsoluteError()
         self.test_mae = torchmetrics.MeanAbsoluteError()
@@ -60,24 +68,26 @@ class MLP_regressor(L.LightningModule):
         self.val_r2 = torchmetrics.R2Score()
         self.test_r2 = torchmetrics.R2Score()
         
-    
+    # Fonction de passe dans le NN
     def forward(self, x):
-        x = self.layers(x)
-        x = self.out(x)
+        x = self.layers(x) # Passe par toutes les couches
+        x = self.out(x) # Puis la couche de sortie
         return x
     
+    # Fonction d'entrainement pour une batch
     def training_step(self, batch, batch_idx):
         x, y = batch
         y = y.view(-1, 1)
-        y_hat = self.forward(x)
-        loss = self.criterion(y_hat, y)
-        if len(y) > 1:
+        y_hat = self.forward(x) # Prédiction
+        loss = self.criterion(y_hat, y) # Perte
+        if len(y) > 1: # Log
             self.log_dict({'train_loss': loss,
                         'train_mae': self.train_mae(y_hat, y),
                         'train_rmse': self.train_rmse(y_hat, y),
                         'train_r2': self.train_r2(y_hat, y)})
-        return loss
+        return loss # Retourne la perte
     
+    # Validation, pas de retro propagation
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y = y.view(-1, 1)
@@ -90,6 +100,7 @@ class MLP_regressor(L.LightningModule):
                         'val_r2': self.val_r2(y_hat, y)})
         return loss
     
+    # Test
     def test_step(self, batch, batch_idx):
         x, y = batch
         y = y.view(-1, 1)
@@ -102,6 +113,7 @@ class MLP_regressor(L.LightningModule):
                         'test_r2': self.test_r2(y_hat, y)})
         return loss
     
+    # Configuration de l'optimizer
     def configure_optimizers(self):
-        optimizer =  self.optimizer(self.parameters(), lr=self.lr)
+        optimizer =  self.optimizer(self.parameters(), lr=self.lr) # Prend en entrée les paramètres et le learning rate
         return optimizer
--- MLP_regression/train.py
+++ MLP_regression/train.py
@@ -12,18 +12,26 @@ import time
 from model import MLP_regressor
 from data import DataModule
 
-
-if torch.backends.mps.is_available():
+# Permets d'utiliser le GPU
+if torch.backends.mps.is_available(): # MPS pour mac
     accelerator = "mps"
-elif torch.cuda.is_available():
+elif torch.cuda.is_available(): # Cuda sinon
     accelerator = "gpu"
 else:
-    accelerator = "cpu"
+    accelerator = "cpu" # CPU si Cuda pas supporté
 
+# Fonction d'optimisation des paramètres et d'entrainement
+# Entrée : 
+# - Chemin d'accès au csv
+# - Dossier où mettre les logs
 def run_trainings(data_path, log_dir="MLP_regression/tb_logs/"):
-    input_size = len(pd.read_csv(data_path).columns) - 1
+    input_size = len(pd.read_csv(data_path).columns) - 1 # Nombre de colonnes - target
     dataset_name = os.path.splitext(os.path.basename(data_path))[0]
+
+    # Fonction d'optimisation Optuna
     def objective(trial):
+
+        # Paramètres optimisés
         n_layer = trial.suggest_int('n_layer', 1, 3)
         n_units = trial.suggest_categorical('n_units', [32, 64, 128, 256, 512])
         decroissant = trial.suggest_categorical('decroissant', [True, False])
@@ -33,45 +41,55 @@ def run_trainings(data_path, log_dir="MLP_regression/tb_logs/"):
         optimizer = trial.suggest_categorical('optimizer', ['Adam'])
         activation = trial.suggest_categorical('activation', ['ReLU', 'tanh', 'sigmoid'])
 
-
+        # Initialisation du modèle avec les paramètres choisis (voir model.py)
         model = MLP_regressor(input_size, output_dim=1, n_layer=n_layer, n_units=n_units, learning_rate=learning_rate, decroissant=decroissant, activation=activation, optimizer=optimizer, criterion=criterion)
+
+        # Dataset et dataloader (voir data.py)
         dm = DataModule(csv_path=data_path, batch_size=batch_size)
 
+        # Log de chaque trial
         logger = TensorBoardLogger(f"{log_dir}{dataset_name}", name=f"trial_{trial.number}")
 
+        # Prunning des trials qui convergent trop lentement par rapport aux autres
         pruning_callback = PyTorchLightningPruningCallback(
             trial, monitor="val_loss"
         )
 
+        # Fonction qui execute la boucle d'entrainement
         trainer = L.Trainer(
-            max_epochs=max_epoch,
-            accelerator=accelerator,
-            callbacks=[EarlyStopping(monitor='val_loss', patience=5), pruning_callback],
-            logger=logger,
-            enable_checkpointing=False
+            max_epochs=max_epoch, # Nombre d'epoch maximum
+            accelerator=accelerator, # GPU si possible
+            callbacks=[EarlyStopping(monitor='val_loss', patience=5), # Early stopping 
+                       pruning_callback], # Prunning
+            logger=logger, # Log
+            enable_checkpointing=False # Ne stocke pas les poids
         )
 
+        # Log les paramètres
         logger.experiment.add_text("hyperparameters", 
             f"batch_size: {batch_size}, learning_rate: {learning_rate}, n_layer: {n_layer}, n_units: {n_units}")
         logger.experiment.add_text("architecture", str(model))
         logger.experiment.add_scalar("parameters", sum(p.numel() for p in model.parameters()))
 
+        # Entrainement du trial, validation et test
         trainer.fit(model, dm)
-        val_result = trainer.validate(model, datamodule=dm)
+        val_result = trainer.validate(model, datamodule=dm) 
         val_loss = val_result[0]['val_loss']
         trainer.test(model, datamodule=dm)
         
-        return val_loss
+        return val_loss # Retourne la perte du dataset de validation
     
-    start_time = time.time()
+    start_time = time.time() # Chronomètre
 
-    study = optuna.create_study(direction='minimize')
-    study.optimize(objective, n_trials=100)
+    # Optimisation
+    study = optuna.create_study(direction='minimize') # On cherche à minimiser la sortie de objective donc la perte de la validation
+    study.optimize(objective, n_trials=100) # 100 trials
 
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Optimization completed in {elapsed_time:.2f} seconds for dataset {dataset_name}.")
 
+    # Stock les meilleurs paramètres
     print("Best trial:")
     trial = study.best_trial
 
